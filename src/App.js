eef3746f3b59ebb2c7e04a2d14a21abd6975037a
Dmitriy <dimaarion@gmail.com>
2023-03-15 16:27:16 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import "./bootstrap/bootstrap.css";
+import "./styles.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
+import ArrowLeft from "./components/ArrowLeft";
 export default function App() {
-  const [getImage, setImage] = useState([{}]);
+  const [getText, setText] = useState({});
 
   useEffect(() => {
-    axios.get("./image.json").then((response) => setImage(response.data));
-  }, [getImage]);
+    axios.get("./text.json").then((response) => setText(response.data));
+  }, [getText]);
 
   return (
-    <div className="App">
-      <h2>Отзывы наших клиентов</h2>
-      <Row>
-        {getImage.map((image, i) => (
-          <Col key={image.name + i}>{image.name}</Col>
-        ))}
-      </Row>
-    </div>
+    <Container fluid className="App text-center">
+      <h2 className="my-3">Отзывы наших клиентов</h2>
+      <Container>
+        <Col className="position-relative">
+          <div className="item item-left position-absolute">{getText.txt}</div>
+          <div className="item item-center position-absolute  active">
+            {getText.txt}
+          </div>
+          <div className="item item-right position-absolute">{getText.txt}</div>
+          <div className="btn">
+            <ArrowLeft />
+          </div>
+        </Col>
+      </Container>
+    </Container>
   );
 }
