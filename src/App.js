8fc7a7055ece93e3cf9877b437d61930ab255f49
Dmitriy <dimaarion@gmail.com>
2023-03-15 10:41:03 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./styles.css";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 export default function App() {
+  const [getImage, setImage] = useState([{}]);
+
+  useEffect(() => {
+    axios.get("./image.json").then((response) => setImage(response.data));
+  }, [getImage]);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Отзывы наших клиентов</h2>
+      <div className="row">
+        {getImage.map((image, i) => (
+          <div className="col" key={image.name + i}>
+            {image.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
